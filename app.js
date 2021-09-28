233f6e7b24bd8b581e793f964f91c0cbf10bd1d6
raj <imman1704@gmail.com>
2021-09-28 19:01:34 +0530

    datatypes

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 console.log("Hello World")
 console.warn("Hello Warning")
 console.error("Hello Error")
+//Datatypes
+//1.String-collection of characters
+console.log(typeof "Hello")
+console.log(typeof 'A')
+//typeof used to find datatype 
+//2.Number datatype
+console.log(typeof 1)
+console.log(typeof 1.322)
+console.log(typeof 1111111111)
+//3.Boolean datatype
+console.log(typeof true)
+console.log(typeof false)
+//Variable declaration
+var age=19
+console.log(age)
