290b7be2fa8a1acc722869fc8a548a12d7654b0f
raj <imman1704@gmail.com>
2021-09-29 19:16:50 +0530

    methods

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var heading=document.getElementById('heading')
-heading.innerHTML='Hi from js'
+var para=document.getElementsByClassName('para')
 console.log(heading)
+console.log(para.length)
+
+var tags=document.getElementsByTagName('a')
+console.log(tags[0])
+
+var query=document.querySelectorAll('.para')
+console.log(query)
