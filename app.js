0178e1f6957e8d981830c4fa0a4bfa691d2df156
raj <imman1704@gmail.com>
2021-09-30 19:43:17 +0530

    foreach

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var arr=['hi','hello','goodmorning']
-var callbackfunction=()=>console.log('Goodmorning')
+var callbackfunction=(a,b)=>console.log(a,b)
 arr.forEach(callbackfunction)
+//prints elements along with index
 
-
+arr.forEach((element, index) => {
+  console.log(element, index)
+})
