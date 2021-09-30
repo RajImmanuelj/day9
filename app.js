2b60a4f183d2c521f9ed934d11dbe48d54afc9b6
raj <imman1704@gmail.com>
2021-09-30 19:35:07 +0530

    foreach

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-var heading = document.getElementById('heading')
-var body=document.querySelector('body')
+var arr=['hi','hello','goodmorning']
+var callbackfunction=()=>console.log('Goodmorning')
+arr.forEach(callbackfunction)
 
 
-console.log(heading)
-
-heading.innerHTML = "i was added by JS"
-heading.style = "color:red;font-size:2rem"
-heading.classList.add('five')
-heading.classList.remove('one')
-body.classList.remove('dark')
-
-console.log(heading.classList)
