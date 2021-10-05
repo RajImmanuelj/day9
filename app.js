ef0368affccd3a660955f2ebf3f98a37208b5971
raj <imman1704@gmail.com>
2021-10-05 18:49:34 +0530

    object

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
-const a=document.querySelectorAll('input')[0]
-const b=document.querySelectorAll('input')[1]
-const add=document.querySelector('#add')
-const multiply=document.querySelector('#multiply')
-const minus=document.querySelector('#sub')
-const result=document.querySelector('.result')
-console.log(result)
-
-//const sum=()=>{
- // const add=parseInt(a.value)+parseInt(b.value)
-//  result.innerHTML=add
-//}
-//add.addEventListener('click',sum)
-
-//const mul=()=>{
- // const product=parseInt(a.value)*parseInt(b.value)
- // result.innerHTML=product
-//}
-//multiply.addEventListener('click',mul)
-//instead of using two many functions we can use single function
-const calculate = (event, operation) => {
-  console.log(event.target)
-  if (operation == 'add') {
-      console.log('adding')
-  } else if (operation == 'multiply') {
-      console.log('multiply')
-  }
-  else if(operation=='sub'){
-    console.log('sub')
-  }
+let car = {
+    name: "C Class",
+    manufacturer: "Mercedes",
+    print:  ()=> {
+        console.log(this)
+        console.log(`${this.name} was created by ${this.manufacturer}`)
+    }
 }
 
+console.log(Object.keys(car))
+console.log(Object.values(car))
+var data = fetch('https://jsonplaceholder.typicode.com/todos/10')
 
-add.addEventListener('click', (event) => {
-  calculate(event, 'add')
-})
-multiply.addEventListener('click', (event) => {
-  calculate(event, 'multiply')
-})
-minus.addEventListener('click',(event)=>{
-  calculate(event,'sub')
-})
+console.log(data)
+
