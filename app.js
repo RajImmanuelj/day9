e3d8f8e8d9c32a0fabe68b48e6f9ebe13ddc6646
raj <imman1704@gmail.com>
2021-10-06 19:25:23 +0530

    await

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-let value=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve('hello')
-    },1000)
-})
-console.log(value)
-setTimeout(()=>{
-    console.log(value)
-},2000)
+const fetchdata=async()=>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/users'
+    )
+    console.log(response)
+    const data=await response.json()
+    console.log(data)
+}
+fetchdata()
