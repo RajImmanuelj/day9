bf91b5bdbc9af44494ce9bf643f070415d094819
raj <imman1704@gmail.com>
2021-10-11 18:17:39 +0530

    json methods

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const button = document.querySelector('button')
 const input = document.querySelector('input')
 const div = document.querySelector('div')
-
-button.addEventListener('click', (req, res) => {
-    localStorage.setItem('InputValue', input.value)
-    div.innerHTML = input.value
-})
-
-const save =localStorage.getItem('InputValue')
-div.innerHTML = save
-
+const arr=[1,2,3,4,5]
+const val=JSON.stringify(arr)
+localStorage.setItem('coverted',val)
+console.log(localStorage.getItem('val'))
